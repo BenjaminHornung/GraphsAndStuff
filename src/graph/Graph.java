dd8662678d5dbccbedf7c649baa5b9cfdb25ec72
ELISABETH\benni <benni.hornung@gmail.com>
2018-05-09 13:28:02 +0200

    initial creation of the graph and vertex classes

---
+++ src/graph/Graph.java
@@ -0,0 +1,51 @@
+package graph;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Graph {
+
+    private int capacity = 10;
+    private int size = 0;
+    private Vertex[] vertices = new Vertex[1];
+
+    public Graph() {
+    }
+
+    public Graph(Vertex ver) throws Exception {
+        addVertex(ver);
+    }
+
+    public Graph(Vertex[] ver) throws Exception {
+        for (Vertex v : ver) {
+            addVertex(v);
+        }
+    }
+
+    public Graph(List<Vertex> ver) throws Exception {
+        for (Vertex v : ver) {
+            addVertex(v);
+        }
+    }
+
+    private void addVertex(Vertex vert) throws Exception {
+        if (checkNames(vert.getName())) {
+            if (this.size == capacity) {
+                vertices = Arrays.copyOf(vertices, (capacity / 2) * 3);
+                capacity = (capacity / 2) * 3;
+            }
+            vertices[size] = vert;
+            size++;
+        } else {
+            throw new Exception("Graph.addVertex: The name of a Vertex must be unique");
+        }
+    }
+
+    public boolean checkNames(String name) {
+        for (Vertex v : vertices) {
+            if (v.getName().equals(name))
+                return false;
+        }
+        return true;
+    }
+}
---
+++ src/graph/Vertex.java
@@ -0,0 +1,77 @@
+package graph;
+
+import java.util.ArrayList;
+
+public class Vertex {
+    public static final int DIRECTED = 0;
+    public static final int UNDIRECTED = 1;
+
+    private int distance = 0;
+    private Vertex parent = null;
+    private ArrayList<Vertex> connectedTo = null;
+    private int state = 0;
+    private String name = null;
+
+
+    public Vertex(String name) {
+        setName(name);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public Vertex getParent() {
+        return parent;
+    }
+
+    public void setParent(Vertex parent) {
+        this.parent = parent;
+    }
+
+    public ArrayList<Vertex> getConnectedTo() {
+        return connectedTo;
+    }
+
+    private void setConnectedTo(Vertex ver) {
+        this.connectedTo.add(ver);
+    }
+
+    public void addConnection(Vertex ver, int type) throws Exception {
+        if (type == DIRECTED) {
+            this.setConnectedTo(ver);
+        } else if (type == UNDIRECTED) {
+            ver.setConnectedTo(this);
+            this.setConnectedTo(ver);
+        } else {
+            throw new Exception("Vertex.addConnection: A connection can be either directed or undirected");
+        }
+    }
+
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) throws Exception {
+        if (this.state > 2) {
+            throw new Exception("Vertex.setState: The state cannot be bigger than 2");
+        } else {
+            this.state = state;
+        }
+    }
+
+}
